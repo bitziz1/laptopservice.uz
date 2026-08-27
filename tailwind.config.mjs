204d9ecd435bd8efca035f9d91cf4fe1a3bba55d
winuser <admin@choyxona.tech>
2026-08-27 21:43:48 +0500

    fix(tailwind): import @tailwindcss/typography plugin to enable prose styles

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,4 +1,6 @@
 /** @type {import('tailwindcss').Config} */
+import typography from '@tailwindcss/typography';
+
 export default {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   theme: {
@@ -35,5 +37,5 @@ export default {
       }
     },
   },
-  plugins: [],
+  plugins: [typography],
 }
