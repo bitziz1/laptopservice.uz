f2eeb274bb0d3df44bb4b7b9d628258f849a3e5e
bitziz1 <azizbekefimov@gmail.com>
2026-08-28 05:41:53 +0000

    Deploying to gh-pages from @ bitziz1/laptopservice.uz@0d93ac39cc27e4472e812a1ade820e5cfa63cce0 🚀

---
+++ _astro/swimlanesDiagram-VR7AAH4N.D8DAIFBe.js
@@ -0,0 +1,8 @@
+import{c as r,s as e}from"./flowDiagram-HODETNUW.DtBwmzFG.js";import{_ as a}from"./mermaid.core.DdAmbSbF.js";import"./chunk-5VM5RSS4.D_YNEqx6.js";import"./chunk-XXDRQBXY.QNjbptop.js";import"./chunk-POPQ4Y6H.C-ZU1uvB.js";import"./chunk-F27PBJKO.BNHPWkLL.js";import"./channel.lHUJhYDw.js";import"./Mermaid.astro_astro_type_script_index_0_lang.VLu7ru-2.js";var o=a(t=>`${e(t)}
+  .swimlane.cluster rect {
+    stroke: ${t.clusterBorder} !important;
+  }
+  [data-look="neo"].cluster rect {
+    filter: none;
+  }
+`,"getStyles"),s=o,n=r({defaultLayout:"swimlane",styles:s});export{n as diagram};
